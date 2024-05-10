76d3a23c16f31413e3116f839f7d9209dd8a2cf0
TimiPashov <pashov91@gmail.com>
2024-05-10 14:54:35 +0300

    add flex-wrap to catalog

--- ReactSellCars/src/components/Catalog/Catalog.tsx
+++ ReactSellCars/src/components/Catalog/Catalog.tsx
@@ -3,27 +3,22 @@ import { useCarContext } from "../../contexts/CarContext";
 import { Car } from "../../types/Car";
 import CarPartial from "../../partials/CarPartial";
 
-
 export default function Catalog() {
   const [result, setResult] = useState<Car[]>([]);
   const [loading, setLoading] = useState(true);
 
-  const { cars } = useCarContext();
- 
-
-  // console.log('from catalog')
+  const { cars } = useCarContext() as { cars: Car[] };
 
   useEffect(() => {
     setResult(cars);
     setLoading(false);
   }, [cars]);
 
-  
   if (loading) return <h1>Loading...</h1>;
   return (
     <div className="mt-14 flex flex-col flex-wrap gap-10">
       <h1 className="text-3xl font-bold self-center">Catalog</h1>
-      <div className="flex gap-4">
+      <div className="flex gap-4 flex-wrap">
         {result.map((car) => (
           <CarPartial key={car._id} car={car} />
         ))}
